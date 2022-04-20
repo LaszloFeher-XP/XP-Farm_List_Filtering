fb98ccad1b2138234daaf2b0d6908ff454657ba9
Laszlo Feher <Laszlo.Feher@ibm.com>
2022-04-20 15:55:43 +0200

    feat: refactored, fixed notes

--- src/kata.js
+++ src/kata.js
@@ -1,10 +1,7 @@
 class Kata {
 
   filter_list(l) {
-    return l.filter(c => this.isNumeric(c));
-  }
-  isNumeric(n) {
-    return (typeof n !== 'string');
+    return l.filter(c => typeof c === 'number');
   }
 }
 
